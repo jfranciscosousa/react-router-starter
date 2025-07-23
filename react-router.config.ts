4cf574928a71c9c8717b9b6a9bed1aff19d8d87b
Francisco Sousa <francisco.sousa@hey.com>
2025-07-23 23:46:25 +0100

    Vercel

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,6 +1,8 @@
+import { vercelPreset } from "@vercel/react-router/vite";
 import type { Config } from "@react-router/dev/config";
 
 export default {
   future: { unstable_optimizeDeps: true },
   ssr: true,
+  presets: [vercelPreset()],
 } satisfies Config;
